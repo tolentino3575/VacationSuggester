b0169d12f7dd92c0e698890845474c52811fc88e
erik <student@epicodus.com>
2016-01-15 09:56:21 -0800

    Add image, edit index.html, make javascript work

--- js/scripts.js
+++ js/scripts.js
@@ -2,12 +2,17 @@ $(document).ready(function() {
   $("form#vacation").submit(function(event) {
     var type = $("select#type").val();
 
-    if (type === "party") {
+    if (type === "history") {
+      $('#london').hide();
+      $('#london').show();
+    } else if (type === "party") {
+      $('#maui').hide();
       $('#maui').show();
-    } 
+    } else if (type === "culture") {
+      $('#hk').hide();
+      $('#hk').show();
+    }
 
-
+    event.preventDefault();
   });
-
-  event.preventDefault();
 });
