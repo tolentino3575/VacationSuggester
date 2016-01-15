a5b9eb58f3c13d41a16ace5c9a35f846b21fcc81
erik <student@epicodus.com>
2016-01-15 14:41:33 -0800

    Edit javascript and css

--- js/scripts.js
+++ js/scripts.js
@@ -8,17 +8,29 @@ $(document).ready(function() {
 
 
     if (history === "yesHistory") {
-      $('.london').show();
+      $('#london').show();
+    } else if (history === "noHistory") {
+      $('#hk').show();
+    } else if (culture === "yesCulture") {
+      $('#maui').show();
     } else if (culture === "noCulture") {
-      $('.london').show();
+      $('#hk').show();
     } else if (food === "yesFood") {
-      $('.hk').show();
+      $('#hk').show();
+    } else if (food === "noFood") {
+      $('#london').show();
+    } else if (outdoor === "yesOut") {
+      $('#hk').show();
     } else if (outdoor === "noOut") {
-      $('.hk').show();
+      $('#maui').show();
+    } else if (beach === "yesBeach") {
+      $('#hk').show();
+    } else if (outdoor === "noBeach") {
+      $('#hk').show();
     } else {
-      $('.maui').show();
+      $('#maui').show();
     }
-    
+
 
 
     //  if (culture === "yesCulture") {
